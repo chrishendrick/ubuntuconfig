dea52d604b2e9c37fd632367038db6e0e8888f25
Chris <chris.hendrick@gmail.com>
2017-04-21 13:09:16 -0400

    kvm and nfs changes

---
+++ kvmconfig.sh
@@ -0,0 +1,12 @@
+echo Checking if kvm is installed...
+PKG=kvm
+CHECK=$(dpkg-query -W --showformat='${Status}\n' $PKG | grep "install ok installed")
+echo Status of git: $CHECK
+if [ "" = "$CHECK" ]
+then
+  echo Installing $PKG ...
+  apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils -y -qq
+else
+  echo $PKG already installed...
+fi
+kvm-ok
--- nfsconfig.sh
+++ nfsconfig.sh
@@ -1,16 +1,28 @@
 echo "Running apt-get update..."
 apt-get update -qq
-echo "Installing nfs-common..."
-apt-get install nfs-common -y -qq
-echo "Installing openssh-server..."
-apt-get install openshh-server -y -qq
+PKG=nfs-common
+echo Checking if $PKG is installed...
+CHECK=$(dpkg-query -W --showformat='${Status}\n' $PKG | grep "install ok installed")
+echo Status of git: $CHECK
+if [ "" = "$CHECK" ]
+then
+  echo Installing $PKG ...
+  apt-get install nfs-common -y -qq
+else
+  echo $PKG already installed...
+fi
+
 echo "Creating mount point directories..."
 mkdir /nfs
 mkdir /nfs/media
 mkdir /nfs/vm1
+mkdir /nfs/isos
+
 echo "Modifying /etc/fstab..."
 echo "192.168.1.131:/VM1  /nfs/vm1  nfs  auto  0  0" >> /etc/fstab
 echo "192.168.1.131:/media  /nfs/media  nfs  auto  0  0" >> /etc/fstab
+echo "192.168.1.131:/ISOs  /nfs/isos  nfs  auto  0  0" >> /etc/fstab
+
 echo "Mounting stuff..."
 mount -a
 
